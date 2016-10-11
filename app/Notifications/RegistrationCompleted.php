6f54e362768040ec1e8d740f9b89784574816308
Karl Hughes <khughes.me@gmail.com>
2016-10-11 13:01:31 -0500

    Basic notification example

---
+++ app/Notifications/RegistrationCompleted.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class RegistrationCompleted extends Notification
+{
+    public $data;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line("Hi {$notifiable->name},")
+            ->line("Your registration is now complete. Thanks, and don't forget to come back to our site!")
+            ->action($this->data['link']['text'], $this->data['link']['url'])
+            ->line("Have a great day!");
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Mail\RegistrationCompleted;
+use App\Mail\RegistrationCompleted as Email;
+use App\Notifications\RegistrationCompleted as Notification;
+use App\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('mail', function () {
-
-    // Using faker for some faux data
-    $faker = Faker\Factory::create();
 
     // This is the data for our Mailer
     $data = [
@@ -28,5 +27,33 @@ Artisan::command('mail', function () {
     ];
 
     // Send our mail
-    Mail::send(new RegistrationCompleted($data));
+    Mail::send(new Email($data));
+});
+
+Artisan::command('notify', function () {
+
+    // Using faker for some faux data
+    $faker = Faker\Factory::create();
+
+    // Instantiate a new user object
+    $user = new User([
+        'name' => $faker->name(),
+        'email' => 'khughes.me@gmail.com',
+    ]);
+
+    $data = [
+        'from' => [
+            'first_name' => $faker->firstName(),
+            'company' => $faker->company(),
+            'email' => $faker->email(),
+            'address' => $faker->address(),
+        ],
+        'link' => [
+            'url' => $faker->url(),
+            'text' => $faker->sentence(),
+        ],
+    ];
+
+    // Send our mail
+    $user->notify(new Notification($data));
 });
